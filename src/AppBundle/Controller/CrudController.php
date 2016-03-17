389bbe3eea99878d436b2daaed55aef6536d7c20
Jacob <hawkwrz@gmail.com>
2016-03-17 13:46:07 +0100

    dodanie obslugi views

--- src/AppBundle/Controller/CrudController.php
+++ src/AppBundle/Controller/CrudController.php
@@ -11,8 +11,29 @@ class CrudController extends Controller
     /**
      * @Route("/crud", name="crud")
      */
-    public function listAction(Request $request)
+    public function listAction()
     {
         return $this->render('crud/index.html.twig');
+    }
+     /**
+     * @Route("/crud/create", name="crud_create")
+     */
+    public function createAction(Request $request)
+    {
+        return $this->render('crud/create.html.twig');
+    }
+     /**
+     * @Route("/crud/edit/{id}", name="crud_edit")
+     */
+    public function editAction($id, Request $request)
+    {
+        return $this->render('crud/index.html.twig');
+    }
+     /**
+     * @Route("/crud/details/{id}", name="crud_details")
+     */
+    public function detailsAction ($id)
+    {
+        return $this->render('crud/details.html.twig');
     }
 }
