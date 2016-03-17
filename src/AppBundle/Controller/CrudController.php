1f940333f4c6ecbdf2f1906d110bf9f981e0a5db
Jacob <hawkwrz@gmail.com>
2016-03-17 13:19:59 +0100

    Nowy Controller do CRUD'a

---
+++ src/AppBundle/Controller/CrudController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CrudController extends Controller
+{
+    /**
+     * @Route("/crud", name="crud")
+     */
+    public function indexAction(Request $request)
+    {
+        die('CRUD');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
