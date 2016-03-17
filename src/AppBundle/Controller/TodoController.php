06b880b678ac8563efca0691554e2df5eb1305b5
Jacob <hawkwrz@gmail.com>
2016-03-17 15:29:04 +0100

    obsluga add, by dzialalo list

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class TodoController extends Controller
 {
@@ -13,13 +19,35 @@ class TodoController extends Controller
      */
     public function listAction()
     {
-        return $this->render('todo/index.html.twig');
+        $todos = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->findAll();
+
+        return $this->render('todo/index.html.twig', array(
+                'todos' => $todos
+            ));
     }
      /**
      * @Route("/todo/create", name="todo_create")
      */
     public function createAction(Request $request)
     {
+        $todo = new Todo;
+
+        $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr' => array('class'=> 'form-control', 'style' =>'marigin-bottom:15px')))
+            ->add('category', TextType::class, array('attr' => array('class'=> 'form-control', 'style' =>'marigin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class'=> 'form-control', 'style' =>'marigin-bottom:15px')))
+            ->add('priority', ChoiceType::class,  array('choices' => array('Low' => 'Low','Normal'=>'Normal','High'=>'High'), 'attr' => array('class'=> 'form-control', 'style' =>'marigin-bottom:15px')))
+            ->add('due_date', DateTimeType::class, array('attr' => array('class'=> 'form-control', 'style' =>'marigin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isVaild())
+        {
+            die('OK');
+        }           
         return $this->render('todo/create.html.twig');
     }
      /**
