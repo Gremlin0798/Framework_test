8d93e4d66df962758752c7f4cf968918c5418508
Jakub Radwan <kubeinsztain@gmail.com>
2016-03-19 12:10:08 +0100

    'Home' przenosi teraz na stronę domyślną podstawowego projektu symfony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
 {
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/home", name="homepage")
      */
     public function indexAction(Request $request)
     {
